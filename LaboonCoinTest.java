7f59cd7191ce429241328a95031eeae3e4cf624a
Alec Rosenbaum <alecr95@gmail.com>
2017-09-25 10:07:58 -0400

    formatting

--- LaboonCoinTest.java
+++ LaboonCoinTest.java
@@ -10,14 +10,14 @@ public class LaboonCoinTest {
     // Create a new LaboonCoin instance before each test.
     @Before
     public void setup() {
-	_l = new LaboonCoin();
+        _l = new LaboonCoin();
     }
-    
+
     // Assert that creating a new LaboonCoin instance
     // does not return a null reference
     @Test
     public void testLaboonCoinExists() {
-	assertNotNull(_l);
+        assertNotNull(_l);
     }
 
     // Creating a block String with valid data should return
@@ -28,38 +28,38 @@ public class LaboonCoinTest {
     // pipes.
     @Test
     public void testCreateBlockValid() {
-	int prevHash = 0x0;
-	int nonce = 0x16f2;
-	int hash = 0x545ac;
-	String validBlock = _l.createBlock("kitten", prevHash, nonce, hash);
-	assertEquals("kitten|00000000|000016f2|000545ac", validBlock);
+        int prevHash = 0x0;
+        int nonce = 0x16f2;
+        int hash = 0x545ac;
+        String validBlock = _l.createBlock("kitten", prevHash, nonce, hash);
+        assertEquals("kitten|00000000|000016f2|000545ac", validBlock);
     }
 
     // Trying to represent a blockchain which has no blocks
     // in it should just return an empty string.
     @Test
     public void testGetBlockChainNoElements() {
-	// By default, _l.blockchain has no elements in it.
-	// So we can just test it immediately.
-	String blockChain = _l.getBlockChain();
-	assertEquals("", blockChain);
+        // By default, _l.blockchain has no elements in it.
+        // So we can just test it immediately.
+        String blockChain = _l.getBlockChain();
+        assertEquals("", blockChain);
     }
-    
+
 
     // Viewing the blockchain as a full string which has valid
     // elements should include all of the elements.  Note that the
     // final element DOES have a trailing \n!
     @Test
     public void testGetBlockChainElements() {
-	_l.blockchain.add("TESTBLOCK1|00000000|000010e9|000a3cd8");
-	_l.blockchain.add("TESTBLOCK2|000a3cd8|00002ca8|0008ff30");
-	_l.blockchain.add("TESTBLOCK3|0008ff30|00002171|0009f908");
-	String blockChain = _l.getBlockChain();
-	assertEquals("TESTBLOCK1|00000000|000010e9|000a3cd8\nTESTBLOCK2|000a3cd8|00002ca8|0008ff30\nTESTBLOCK3|0008ff30|00002171|0009f908\n", blockChain);
+        _l.blockchain.add("TESTBLOCK1|00000000|000010e9|000a3cd8");
+        _l.blockchain.add("TESTBLOCK2|000a3cd8|00002ca8|0008ff30");
+        _l.blockchain.add("TESTBLOCK3|0008ff30|00002171|0009f908");
+        String blockChain = _l.getBlockChain();
+        assertEquals("TESTBLOCK1|00000000|000010e9|000a3cd8\nTESTBLOCK2|000a3cd8|00002ca8|0008ff30\nTESTBLOCK3|0008ff30|00002171|0009f908\n", blockChain);
     }
-	    
+
     /* USER ADDED TESTS */
-    
+
 
     @Test
     public void hashTest001() {
@@ -87,7 +87,7 @@ public class LaboonCoinTest {
     // final element DOES have a trailing \n!
     @Test
     public void validHashTest001() {
-        assertTrue( _l.validHash(3,0x000fd98a));
+        assertTrue( _l.validHash(3, 0x000fd98a));
     }
 
     // Viewing the blockchain as a full string which has valid
@@ -95,7 +95,7 @@ public class LaboonCoinTest {
     // final element DOES have a trailing \n!
     @Test
     public void validHashTest002() {
-        assertFalse( _l.validHash(2,0x000fd98a));
+        assertFalse( _l.validHash(2, 0x000fd98a));
     }
 
     // Viewing the blockchain as a full string which has valid
@@ -103,7 +103,7 @@ public class LaboonCoinTest {
     // final element DOES have a trailing \n!
     @Test
     public void validHashTest003() {
-        assertTrue( _l.validHash(0,0xab000000));
+        assertTrue( _l.validHash(0, 0xab000000));
     }
 
 }
